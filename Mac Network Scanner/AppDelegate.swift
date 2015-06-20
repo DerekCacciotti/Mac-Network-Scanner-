4902f6461131c4b9eab14e1e369a3b0064ee34e4
Derek Cacciotti <derekcacciotti@Dereks-iMac.local>
2015-06-20 12:30:22 -0400

    Initial Commit

---
+++ Mac Network Scanner/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Mac Network Scanner
+//
+//  Created by Derek Cacciotti on 6/20/15.
+//  Copyright (c) 2015 Derek Cacciotti. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
